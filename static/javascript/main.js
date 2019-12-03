bf2f423951e42427ce843e5f9f7287c31a9c51fc
ehsan mahmoudzadeh <ehsanm@wustl.edu>
2019-12-03 07:45:38 -0600

    more text update

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -7,7 +7,7 @@ require([ 'jquery', 'app', 'main-nav-bar', 'main-view'], function($, app) {
   $mainView.main_view();
   app.ui.mainView = $mainView;
   
-  $mainView.main_view('createView', 'welcome', $('<div style="margin-left: 100px; margin-right:100px"><h1 style="align:center;">QbiQe Project!</h1>This is page is welcoming page!<br><br>Please click on Flowchart at the left bar panel.</div>'));
+  $mainView.main_view('createView', 'welcome', $('<div style="margin-left: 100px; margin-right:100px"><h1 style="align:center;">QbiQe Project!</h1>This is page will populate information as a dashboard!<br><br>Please click on Flowchart at the left bar panel.</div>'));
   $mainView.main_view('showView', 'welcome');
   
   $mainNavBar.main_nav_bar('addButton', 'welcome', 'Main Panel', '', 0, function() {
