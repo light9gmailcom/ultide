0e762966713370c9716ac4fdbbd67bd0874f7367
ehsan mahmoudzadeh <ehsanm@wustl.edu>
2019-12-30 12:27:37 -0600

    update text

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -7,7 +7,7 @@ require([ 'jquery', 'app', 'main-nav-bar', 'main-view'], function($, app) {
   $mainView.main_view();
   app.ui.mainView = $mainView;
   
-  $mainView.main_view('createView', 'welcome', $('<div style="margin-left: 100px; margin-right:100px"><h1 style="align:center;">QbiQe Project!</h1>This is page is dashboard. You can find all result from the last resutl here!<br><br>Please click on Design at the left bar panel.</div>'));
+  $mainView.main_view('createView', 'welcome', $('<div style="margin-left: 100px; margin-right:100px"><h1 style="align:center;">Project XYZ!</h1>This is page is dashboard. You can find all result from the last resutl here!<br><br>Please click on Design at the left bar panel.</div>'));
   $mainView.main_view('showView', 'welcome');
   
   $mainNavBar.main_nav_bar('addButton', 'welcome', 'Dashboard Panel', '', 0, function() {
